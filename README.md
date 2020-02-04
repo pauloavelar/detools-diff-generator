61a7756eb36e788697a77af0fd1687e9c5500e6b
Paulo Avelar <paulo.avelar@mercadolivre.com>
2020-02-04 10:52:49 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Diff Generator
+
+Steps:
+
+1. Paste all binary files into the `files` directory, **version-ordered alphabetically**.
+2. Install dependencies with `pip3 install -r requirements.txt`.
+3. Run the generator with `python3 diff.py`.
+4. Look for the generated files in the `out` directory.
+
+> Keep the filenames short, they will be used as part of the names on the generated files.
---
+++ diff.py
@@ -0,0 +1,27 @@
+import detools
+import os
+
+files = [f for f in os.listdir('files') if f != '.gitkeep']
+
+def get_filename(f):
+  return os.path.splitext(f)[0]
+
+def from_files(name):
+  return f'files/{name}'
+
+if len(files) < 2:
+  print('Not enough files')
+  exit
+
+files.sort()
+
+for i in range(0, len(files)):
+  old = files[i]
+
+  for j in range(i + 1, len(files)):
+    new = files[j]
+
+    diff = f'out/diff_{get_filename(old)}_{get_filename(new)}.ptm'
+
+    print(f'Generating "{diff}"...')
+    detools.create_patch_filenames(from_files(old), from_files(new), diff, compression='heatshrink')
